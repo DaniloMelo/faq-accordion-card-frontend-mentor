d8f3414b8f2c4a70df6c3de5fba06105b07fb2ef
Danilo Melo <danilomarquesdemelo@gmail.com>
2023-03-03 16:42:53 -0300

    Added bolder CSS class to elements when clicked

--- src/js/script.js
+++ src/js/script.js
@@ -1,51 +1,12 @@
-const arrows = document.querySelectorAll('.arrow')
-
-
-// questions.forEach( question => {
-//     question.addEventListener('click', (event) => {
-//         question.lastElementChild.classList.toggle('faq__answer--hide')
-
-//         console.log(event.target);
-//     })
-// })
-
-// arrows.forEach( arrow => {
-    //     a
-    // })
-
-
-
-//////////////////////////
-
-
-// const questions = document.querySelectorAll('.faq__question')
-
-// questions.forEach( question => {
-//     question.addEventListener('click', (event)  => {
-
-//         if(event.target.parentNode.classList.contains('faq__question')){
-//             event.target.nextElementSibling.classList.toggle('faq__answer--hide')
-//         }
-        
-//     })
-// })
-
-
 const faqQuestionsBoxes = document.querySelectorAll('.faq__question')
 
 faqQuestionsBoxes.forEach( box => {
     box.addEventListener('click', (event) => {
         box.classList.toggle('faq__answer--toggle')
         box.classList.toggle('faq__anwer--rotate-arrow')
-        // console.log(event.target.childNodes[1])
-        // console.log(box.classList.contains('faq__question'))
-
-        // if(box.classList.contains('faq__question')){
-        //     box.classList.toggle('faq__answer--toggle')
-        //     event.target.childNodes[1].classList.toggle('faq__anwer--rotate-arrow')
-        // }
-
-        
+        box.firstElementChild.classList.toggle('faq__question--bold')
+        console.log(box)
+ 
     })
 })
 
